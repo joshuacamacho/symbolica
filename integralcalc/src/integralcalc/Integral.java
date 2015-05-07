44921ce29f273f66ac6d6531339f6ceb8d40520d
joshuacamacho <joshuajcamacho@gmail.com>
2015-05-07 16:52:13 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- integralcalc/src/integralcalc/Integral.java
+++ integralcalc/src/integralcalc/Integral.java
@@ -1,10 +1,24 @@
 package integralcalc;
 
 public class Integral {
+	String s;
+	String sIntegral;
 	public Integral(){
-		
 	}
-	public void eval(String s){
-		
+	public void eval(String str){
+		this.s=str;
+	}
+	public String commonCheck(String str){
+		String ret="";
+		if(str=="cos(x)"){
+			ret= "sin(x)";
+		}else if(str=="sin(x)"){
+			ret= "-cos(x)";
+		}else if(str=="1/x"){
+			ret= "ln(x)";
+		}else if(str=="1/1+x"){
+			ret= "tan(x)";
+		}
+		return ret;
 	}
 }
