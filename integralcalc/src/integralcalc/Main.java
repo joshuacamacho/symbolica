0d240cb1690f0b375fac28dd1edb7d41c7859261
joshuacamacho <joshuajcamacho@gmail.com>
2015-05-13 16:40:51 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- integralcalc/src/integralcalc/Main.java
+++ integralcalc/src/integralcalc/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integral i=new Integral();
-		i.eval("cos(x)+sin(x)");
-		i.eval("3*cos(x)");
+		i.eval("cos(x)+sin(x)+1/x+1/x+cos(x)");
+		i.eval("3*1/x");
 		
 		//i.displayterms();
 		
