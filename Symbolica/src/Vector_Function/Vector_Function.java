528e6577633dc6aac3b3b9511b5d833beffde1bd
TheHashSlingingSlasher <awf4598@yahoo.com>
2015-06-15 15:18:57 -0700

    Basic setter/getter code for Vector Function class

---
+++ Symbolica/src/Vector_Function/Vector_Function.java
@@ -0,0 +1,112 @@
+/*
+ * Name: Alec Farfan
+ * Date: 06/15/15
+ * Purpose: Vector_Function class
+ */
+
+package Vector_Function;
+
+// Import libraries
+import Calculus.Differential.*;
+import Calculus.Integral.*;
+
+/**
+ *
+ * @author Alec
+ */
+public class Vector_Function {
+    
+    // Data fields
+    String []component_functions; // < f(x), g(x), h(x) > ect..
+    int dimension;                // Number of component functions
+    
+    /**
+     * Default constructor for the Vector_Function class. Sets the
+     * component_functions array to null, and the dimension to 0.
+     */
+    public Vector_Function(){
+        
+        component_functions = null;
+        dimension = 0;
+        
+    }
+    
+    /**
+     * Constructor for the Vector_Function class. Sets the dimension field and
+     * initializes the component_functions array to size of dimension.
+     * @param dimension // The dimension of the new vector function
+     */
+    public Vector_Function(int dimension){
+        
+        component_functions = new String[dimension];
+        this.dimension = dimension;
+        
+    }
+    
+    /**
+     * Setter method for the component_functions field. Sets the
+     * component_functions array to the value passed into the parameter.
+     * @param component_functions Array to be assigned to component_functions
+     */
+    public void set_component_functions(String[] component_functions){
+        
+        this.component_functions = component_functions;
+        
+    }
+    
+    /**
+     * Getter method for the component_functions field.
+     * @return The calling object's component_functions field.
+     */
+    public String[] get_component_functions(){
+        
+        return component_functions;
+        
+    }
+    
+    /**
+     * Setter method for the dimensions field. Sets the calling object's 
+     * dimension field to the value passed into the parameter.
+     * @param dimension The value to be assigned to the calling object's
+     *                  dimension field.
+     */
+    public void set_dimension(int dimension){
+        
+        this.dimension = dimension;
+        
+    }
+    
+    /**
+     * Getter method for the dimension field.
+     * @return The calling object's dimension field.
+     */
+    public int get_dimension(){
+        
+        return dimension;
+        
+    }
+    
+    /**
+     * Assigns a function to the component_functions array at a given index.
+     * @param function The component_function to be assigned
+     * @param index    The index to place the function into
+     */
+    public void assign_component(String function, int index){
+        
+        component_functions[index] = function;
+        
+    }
+    
+    /**
+     * Retrieves the component_function at a given position of the Vector
+     * Function.
+     * @param index The position of the Vector function to retrieve from
+     * @return      The component function located at index
+     */
+    public String get_component(int index){
+        
+        return component_functions[index];
+        
+    }
+    
+}
